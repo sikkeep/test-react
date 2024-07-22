2a0b6b386bd69aea209625372f36fda9a3748b92
ivan.oleynik <mcafeeplay@gmail.com>
2024-07-22 11:53:58 +0000

    test

--- lib/auth.ts
+++ lib/auth.ts
@@ -1,8 +1,9 @@
+import type { AuthOptions } from 'next-auth';
 import CredentialsProvider from 'next-auth/providers/credentials';
-import { signInWithEmailAndPassword } from 'firebase/auth';
-import { auth } from '@/lib/firebase';
+// import { signInWithEmailAndPassword } from 'firebase/auth';
+// import { auth } from '@/lib/firebase';
 
-const authOptions = {
+const authOptions: AuthOptions = {
   pages: {
     signIn: '/',
   },
@@ -14,31 +15,32 @@ const authOptions = {
         password: { label: 'Password', type: 'password' },
       },
       authorize: async (credentials): Promise<any> => {
-        try {
-          if (!credentials || !credentials.email || !credentials.password) {
-            throw new Error('Missing email or password');
-          }
+        console.log('Credentials:', credentials);
 
-          console.log('Credentials:', credentials);
+        return;
+        // try {
+        //   if (!credentials || !credentials.email || !credentials.password) {
+        //     throw new Error('Missing email or password');
+        //   }
 
-          const userCredential = await signInWithEmailAndPassword(
-            auth,
-            (credentials as any).email || '',
-            (credentials as any).password || '',
-          );
+        //   const userCredential = await signInWithEmailAndPassword(
+        //     auth,
+        //     (credentials as any).email || '',
+        //     (credentials as any).password || '',
+        //   );
 
-          if (userCredential.user) {
-            return {
-              id: userCredential.user.uid,
-              email: userCredential.user.email,
-            };
-          } else {
-            throw new Error('No user found');
-          }
-        } catch (error) {
-          console.error('Error in authorize:', error);
-          return null;
-        }
+        //   if (userCredential.user) {
+        //     return {
+        //       id: userCredential.user.uid,
+        //       email: userCredential.user.email,
+        //     };
+        //   } else {
+        //     throw new Error('No user found');
+        //   }
+        // } catch (error) {
+        //   console.error('Error in authorize:', error);
+        //   return null;
+        // }
       },
     }),
   ],
--- next.config.mjs
+++ next.config.mjs
@@ -1,19 +1,4 @@
-// next.config.mjs
-
-import path from 'path';
-import { fileURLToPath } from 'url';
-
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = path.dirname(__filename);
-
-const nextConfig = {
-  webpack: (config, { isServer }) => {
-    // Resolve alias @ to the root directory
-    config.resolve.alias['@'] = __dirname;
-    config.externals = [...config.externals, 'bcrypt'];
-
-    return config;
-  },
-};
+/** @type {import('next').NextConfig} */
+const nextConfig = {};
 
 export default nextConfig;
